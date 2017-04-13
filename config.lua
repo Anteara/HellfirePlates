048b0b1bcd36a5ad1b37989006adce8b68d41305
Steamboatt <kevin.mallinson@outlook.com>
2017-04-14 00:09:29 +1000

    Migrated the settings frame to its own self contained XML file.

--- config.lua
+++ config.lua
@@ -1,200 +1,95 @@
-local backdrop = {
-  bgFile = "Interface\\Tooltips\\UI-Tooltip-Background", tile = true, tileSize = 32,
-  insets = {left = 0, right = 0, top = 0, bottom = 0},
+--Settings that the user can configure himself.
+local userConfigurableSettings = 
+{
+	["enableAddonFunctionality"]	= true,
+	["classColourFriendly"]    		= true,
+	["classColourEnemy"]   			= true,
+	["nameplateShowHP"]   			= true,
+	["nameplateShowCastbar"]		= true,
+	["nameplateshowDebuffs"]		= true,
+	["nameplateShowComboPoints"]	= true,
 }
 
-local checkbox = {
-  ["blueshaman"]    = "Enable Blue Shaman Class Color",
-  ["showdebuffs"]   = "Show Debuffs on Target Nameplate",
-  ["showcastbar"]   = "Show Castbar",
-  ["spellname"]     = "Show Spellname On Castbar",
-  ["showhp"]        = "Display HP",
-  ["enemyclassc"]   = "Enable Enemy Class Colors",
-  ["friendclassc"]  = "Enable Friend Class Colors",
+--The class colours for each class, to be shown on the nameplates.
+local classColours =
+{
+	["shaman"] = 
+	{ 
+		r = 0,
+		g = 0.439, 
+		b = 0.871, 
+		hex = "ff0070de" 
+	},
+	["mage"] = 
+	{ 
+		r = 0.412, 
+		g = 0.8, 
+		b = 0.941, 
+		hex = "ff69ccf0" 
+	},
+	["warlock"] = 
+	{ 
+		r = 0.58, 
+		g = 0.51, 
+		b = 0.788, 
+		hex = "ff9482c9" 
+	},
+	["priest"] = 
+	{ 
+		r = 1, 
+		g = 1, 
+		b = 1, 
+		hex = "ffffffff" 
+	},
+	["rogue"] = 
+	{ 
+		r = 1, 
+		g = 0.961, 
+		b = 0.412, 
+		hex = "fffff569" 
+	},
+	["hunter"] = 
+	{ 
+		r = 0.671, 
+		g = 0.831, 
+		b = 0.451, 
+		hex = "ffabd473" 
+	},
+	["paladin"] = 
+	{ 
+		r = 0.961, 
+		g = 0.549, 
+		b = 0.729, 
+		hex = "fff58cba" 
+	},
+	["warrior"] = 
+	{ 
+		r = 0.78, 
+		g = 0.612,
+		b = 0.431, 
+		hex = "ffc79c6e" 
+	},
+	["druid"] = 
+	{ 
+		r = 1, 
+		g = 0.49,
+		b = 0.039, 
+		hex = "ffff7d0a" 
+	}
 }
 
-local text = {
-  ["vpos"]           = "Vertical Offset",
-  ["raidiconsize"]   = "Raid Icon Size",
-}
-
--- config
-pfConfigCreate = CreateFrame("Frame", nil, UIParent)
-pfConfigCreate:RegisterEvent("VARIABLES_LOADED")
-
-function pfConfigCreate:ResetConfig()
-  pfNameplates_config = { }
-  pfNameplates_config["blueshaman"] = "1"
-  pfNameplates_config["raidiconsize"] = "16"
-  pfNameplates_config["showdebuffs"] = "1"
-  pfNameplates_config["showcastbar"] = "1"
-  pfNameplates_config["spellname"] = "1"
-  pfNameplates_config["showhp"] = "0"
-  pfNameplates_config["vpos"] = "0"
-  pfNameplates_config["clickthreshold"] = ".5"
-  pfNameplates_config["enemyclassc"] = "1"
-  pfNameplates_config["friendclassc"] = "1"
-end
-
-pfConfigCreate:SetScript("OnEvent", function()
-  if not pfNameplates_config then
-    pfConfigCreate:ResetConfig()
-  end
-
-  HellfirePlatesConfig:Initialize()
-
-  if pfNameplates_config.blueshaman == "1" then
-    RAID_CLASS_COLORS["SHAMAN"] = { r = 0.14, g = 0.35, b = 1.0, colorStr = "ff0070de" }
-  end
-end)
-
-HellfirePlatesConfig = HellfirePlatesConfig or CreateFrame("Frame", "HellfirePlatesConfig", UIParent)
-function HellfirePlatesConfig:Initialize()
-  HellfirePlatesConfig:Hide()
-  HellfirePlatesConfig:SetBackdrop(backdrop)
-  HellfirePlatesConfig:SetBackdropColor(0,0,0,1)
-  HellfirePlatesConfig:SetWidth(400)
-  HellfirePlatesConfig:SetHeight(500)
-  HellfirePlatesConfig:SetPoint("CENTER", 0, 0)
-  HellfirePlatesConfig:SetMovable(true)
-  HellfirePlatesConfig:EnableMouse(true)
-  HellfirePlatesConfig:SetScript("OnMouseDown",function()
-    HellfirePlatesConfig:StartMoving()
-  end)
-
-  HellfirePlatesConfig:SetScript("OnMouseUp",function()
-    HellfirePlatesConfig:StopMovingOrSizing()
-  end)
-
-  HellfirePlatesConfig.vpos = 60
-
-  HellfirePlatesConfig.title = CreateFrame("Frame", nil, HellfirePlatesConfig)
-  HellfirePlatesConfig.title:SetPoint("TOP", 0, -2);
-  HellfirePlatesConfig.title:SetWidth(396);
-  HellfirePlatesConfig.title:SetHeight(40);
-  HellfirePlatesConfig.title.tex = HellfirePlatesConfig.title:CreateTexture("LOW");
-  HellfirePlatesConfig.title.tex:SetAllPoints();
-  HellfirePlatesConfig.title.tex:SetTexture(0,0,0,.5);
-
-  HellfirePlatesConfig.caption = HellfirePlatesConfig.caption or HellfirePlatesConfig.title:CreateFontString("Status", "LOW", "GameFontWhite")
-  HellfirePlatesConfig.caption:SetPoint("TOP", 0, -10)
-  HellfirePlatesConfig.caption:SetJustifyH("CENTER")
-  HellfirePlatesConfig.caption:SetText("HellfirePlates")
-  HellfirePlatesConfig.caption:SetFont("Interface\\AddOns\\HellfirePlates\\fonts\\arial.ttf", 24)
-  HellfirePlatesConfig.caption:SetTextColor(.2,1,.8,1)
-
-  for config, description in pairs(checkbox) do
-    HellfirePlatesConfig:CreateEntry(config, description, "checkbox")
-  end
-
-  for config, description in pairs(text) do
-    HellfirePlatesConfig:CreateEntry(config, description, "text")
-  end
-
-  HellfirePlatesConfig.reload = CreateFrame("Button", nil, HellfirePlatesConfig, "UIPanelButtonTemplate")
-  HellfirePlatesConfig.reload:SetWidth(150)
-  HellfirePlatesConfig.reload:SetHeight(30)
-  HellfirePlatesConfig.reload:SetNormalTexture(nil)
-  HellfirePlatesConfig.reload:SetHighlightTexture(nil)
-  HellfirePlatesConfig.reload:SetPushedTexture(nil)
-  HellfirePlatesConfig.reload:SetDisabledTexture(nil)
-  HellfirePlatesConfig.reload:SetBackdrop(backdrop)
-  HellfirePlatesConfig.reload:SetBackdropColor(0,0,0,1)
-  HellfirePlatesConfig.reload:SetPoint("BOTTOMRIGHT", -20, 20)
-  HellfirePlatesConfig.reload:SetText("Save")
-  HellfirePlatesConfig.reload:SetScript("OnClick", function()
-    ReloadUI()
-  end)
-
-  HellfirePlatesConfig.reset = CreateFrame("Button", nil, HellfirePlatesConfig, "UIPanelButtonTemplate")
-  HellfirePlatesConfig.reset:SetWidth(150)
-  HellfirePlatesConfig.reset:SetHeight(30)
-  HellfirePlatesConfig.reset:SetNormalTexture(nil)
-  HellfirePlatesConfig.reset:SetHighlightTexture(nil)
-  HellfirePlatesConfig.reset:SetPushedTexture(nil)
-  HellfirePlatesConfig.reset:SetDisabledTexture(nil)
-  HellfirePlatesConfig.reset:SetBackdrop(backdrop)
-  HellfirePlatesConfig.reset:SetBackdropColor(0,0,0,1)
-  HellfirePlatesConfig.reset:SetPoint("BOTTOMLEFT", 20, 20)
-  HellfirePlatesConfig.reset:SetText("Reset")
-  HellfirePlatesConfig.reset:SetScript("OnClick", function()
-    pfNameplates_config = nil
-    ReloadUI()
-  end)
-end
-
-function HellfirePlatesConfig:CreateEntry(config, description, type)
-  -- sanity check
-  if not pfNameplates_config[config] then
-    pfConfigCreate:ResetConfig()
-  end
-
-  -- basic frame
-  local frame = getglobal("SPC" .. config) or CreateFrame("Frame", "SPC" .. config, HellfirePlatesConfig)
-  frame:SetWidth(400)
-  frame:SetHeight(25)
-  frame:SetPoint("TOP", 0, -HellfirePlatesConfig.vpos)
-
-  -- caption
-  frame.caption = frame.caption or frame:CreateFontString("Status", "LOW", "GameFontWhite")
-  frame.caption:SetFont("Interface\\AddOns\\HellfirePlates\\fonts\\arial.ttf", 14)
-  frame.caption:SetPoint("LEFT", 20, 0)
-  frame.caption:SetJustifyH("LEFT")
-  frame.caption:SetText(description)
-
-  -- checkbox
-  if type == "checkbox" then
-    frame.input = frame.input or CreateFrame("CheckButton", nil, frame, "UICheckButtonTemplate")
-    frame.input:SetWidth(24)
-    frame.input:SetHeight(24)
-    frame.input:SetPoint("RIGHT" , -20, 0)
-
-    frame.input.config = config
-    if pfNameplates_config[config] == "1" then
-      frame.input:SetChecked()
-    end
-
-    frame.input:SetScript("OnClick", function ()
-      if this:GetChecked() then
-        pfNameplates_config[this.config] = "1"
-      else
-        pfNameplates_config[this.config] = "0"
-      end
-    end)
-
-  elseif type == "text" then
-    -- input field
-    frame.input = frame.input or CreateFrame("EditBox", nil, frame)
-    frame.input:SetTextColor(.2,1,.8,1)
-    frame.input:SetJustifyH("RIGHT")
-
-    frame.input:SetWidth(50)
-    frame.input:SetHeight(20)
-    frame.input:SetPoint("RIGHT" , -20, 0)
-    frame.input:SetFontObject(GameFontNormal)
-    frame.input:SetAutoFocus(false)
-    frame.input:SetScript("OnEscapePressed", function(self)
-      this:ClearFocus()
-    end)
-
-    frame.input.config = config
-    frame.input:SetText(pfNameplates_config[config])
-
-    frame.input:SetScript("OnTextChanged", function(self)
-      pfNameplates_config[this.config] = this:GetText()
-    end)
-  end
-
-  HellfirePlatesConfig.vpos = HellfirePlatesConfig.vpos + 30
-end
-
+--The various slash commands that can be used to access the configuration frame.
 SLASH_SHAGUPLATES1 = '/hellfireplates'
 SLASH_SHAGUPLATES2 = '/hp'
+SLASH_SHAGUPLATES2 = '/hf'
+SLASH_SHAGUPLATES3 = '/plates'
+SLASH_SHAGUPLATES4 = '/nameplates'
 
+--Function that will show or hide the configuration frame.
 function SlashCmdList.SHAGUPLATES(msg)
-  if HellfirePlatesConfig:IsShown() then
-    HellfirePlatesConfig:Hide()
+  if Hellfire_Options:IsShown() then
+	Hellfire_Options:Hide() --new
   else
-    HellfirePlatesConfig:Show()
+	Hellfire_Options:Show(); --new
   end
 end
